5d47eae0216697ba1a9984ba27068c6935f10cd3
edgarconzam <patricio.zambrano2005@gmail.com>
2023-09-19 23:35:24 -0600

    P003_Pato

---
+++ P003_Mayor_que_Menor_que/P003_Mayor_que_Menor_que.cpp
@@ -0,0 +1,54 @@
+// P003_Mayor_que_Menor_que.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//Edgar Constantino
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1;
+	int num2;
+
+
+
+	cout << "En este programa determinara que numero es mayor que el otro\n";
+	cout << "Usuario dame el primer numero\n";
+	cin >> num1;
+	cout << "Usuario dame el segundo numero\n";
+	cin >> num2;
+
+	if (num1 > num2){
+		cout << "El mayor es: " << num1; 
+		cout << "\n nos vemos pa >:D";
+	}
+
+	else{
+		cout << "El mayor es: " << num2;  
+		cout << "\n nos vemos pa >:D";
+			
+		
+		
+	}
+	
+	
+
+	
+
+}
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
