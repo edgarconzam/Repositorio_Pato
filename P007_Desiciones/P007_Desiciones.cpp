c5de1825f68fed2ca7a8bcf03bb108a384ae406e
edgarconzam <patricio.zambrano2005@gmail.com>
2023-09-26 19:43:16 -0600

    P007_Pato

--- P007_Desiciones/P007_Desiciones.cpp
+++ P007_Desiciones/P007_Desiciones.cpp
@@ -29,16 +29,30 @@ int main()
             std::cin >> tipo_pelea;
 
             switch (tipo_pelea)
+            {
 
             case 1:
-                std::cout << " Elegiste Royal Rumble\n";
-                std::cout << " aqui solo hay una ronda, TODOS VS TODOS osea un 1v1v1v1v1v1v1v1\n";
+                std::cout << " Elegiste Royal Rumble";
+                std::cout << " aqui solo hay una ronda, TODOS VS TODOS osea un 1v1v1v1v1v1v1v1";
+                break;
+
 
             case 2:
-                std::cout << "Elegiste Eliminacion directa\n";
-                std::cout << "son 4 jornadas de 1v1, despues el ganador de cada jornada juega vs el ganador de la otra jornada y quedarian 4 peleadores\n";
-                std::cout << "y en la segunda jornada"
-            
+                std::cout << "Elegiste Eliminacion directa";
+                std::cout << "En la primera jornada habran 4 combates de 1v1, despues el ganador de cada combate juega vs el ganador de la otra jornada y quedarian 4 peleadores\n";
+                std::cout << "y en la segunda jornada se haran dos combates de 1v1, depues el ganador de cada combate pelearan entre si en la 3 jornada de un solo combate  de 1v1 ";
+                break;
+
+
+            case 3:
+                std::cout << "Elegiste Grupos, semifinal y final";
+                std::cout << "En la fase de grupos se haran 2 grupos de 4 y se haran 3 jornadas osea cada peleador tendra 3 combates\n";
+                std::cout << "y solo pasan los 2 mejores peleadores de cada grupo a la siguiente fase que es la semifinal\n";
+                std::cout << "cada peleador ganador de la semifinal peleara en la final, y el ganador de la final sera el completo GANADOR\n";
+                default:
+                    std::cout << "Elige bien la opcion\n";
+
+            }
 
                 
 
@@ -50,6 +64,34 @@ int main()
             std::cout << "escojiste 12 peleadores\n";
             std::cout << "Muy bien ahora dime que sistema de combate te gustaria para esta pelea\n";
             std::cout << "1.- royal rumble  2.- Eliminatoria directa  3.- Grupos, semifinal y final\n";
+            std::cin >> tipo_pelea;
+
+            switch (tipo_pelea)
+            {
+            case 1:
+                std::cout << "Escojisto Royal rumble";
+                std::cout << "aqui solo hay una ronda, TODOS VS TODOS osea un 1v1v1v1v1v1v1v1v1v1v1v1\n";
+                break;
+
+            case 2:
+                std::cout << "Escojiste Eliminatoria directa\n";
+                std::cout << "En la primera jornada habran 6 combates de 1v1, depues el ganador de cada combate juega vs el ganador de la otra jornada y quedarian 6 peleadores\n";
+                std::cout << "y en la segunda jornada se haran 3 combates de 1v1 y quedarian 3 peleadores y en la 4 jornada se hara un combate de 1v1v1 y el gandor sera el ABSOLUTO CAMPEON\n ";
+                break;
+
+            case 3:
+                std::cout << "Elegiste Grupos, semifinal y final";
+                std::cout << "En la fase de grupos se haran 4 grupos de 3 y se hara 2 jornadas cada peleador\n";
+                std::cout << "y solo pasa el mejor peleador de cada grupo a la siguiente fase que es la semifinal\n";
+                std::cout << "cada peleador ganador de la semifinal peleara en la final, y el ganador de la final sera el completo GANADOR\n";
+            default:
+                std::cout << "Elige bien la opcion\n";
+
+
+
+            }
+
+           
 
         }
 
