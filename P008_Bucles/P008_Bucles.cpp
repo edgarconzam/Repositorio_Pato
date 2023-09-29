8f73298c86155deb0e662223854134ff01b62f40
edgarconzam <patricio.zambrano2005@gmail.com>
2023-09-28 18:28:16 -0600

    P008

--- P008_Bucles/P008_Bucles.cpp
+++ P008_Bucles/P008_Bucles.cpp
@@ -8,31 +8,28 @@ using namespace std;
 
 int main()
 {
-    int a = 1;
-    bool seguir = true;
-
-    while (seguir)
-    {
-        cout << "se hara una cuenta del 1 al 10 :D\n";
-
-        do
-        {
-            cout << a << endl;
-            a++; //aumenta el iterador de uno en uno
-        } 
-        while (a <= 10);
-        {
-
-        
-
-        cout << "Repetimos la cuenta? 0.- No  1.- Si\n";
-        cin >> seguir;
-        }
-
-        system("cls");
-        
-    }
+    //Declarar 2 variables a analizar.
+    bool seguir = false;
     
+    //El que se ejecutara si o si 1 vez - Do While
+    do
+    {
+        cout << "Entraste al do-while...0.-No\n1.-Si\n";
+        cin >> seguir;
+    } while (seguir);
+   
+    for (int i = 0; i <= 100; i = i + 1)
+    {
+        cout << "El contador va en: " << i;
+        cout << endl;
+
+    }
+
 }
+
+
+
+
+
 
  
