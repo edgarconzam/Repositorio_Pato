6e4529ac4e8b65b42f2afe100e05ef35b06d552a
edgarconzam <patricio.zambrano2005@gmail.com>
2023-10-01 23:36:58 -0600

    dr

--- P009_Condicionales_Calculadora/P009_Condicionales_Calculadora.cpp
+++ P009_Condicionales_Calculadora/P009_Condicionales_Calculadora.cpp
@@ -2,20 +2,27 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 int main()
 {
     int opc;
     int n1, n2;
-    float VA{};
-    float valorabsoluto = abs(VA);
+    float valor{};
+   
+    bool repetir = true;
 
-        std::cout << "Hola usuario dime que Operacion quieres hacer?\n";
-        std::cout << "1.-suma 2.-resta 3.-multiplicacion 4.-division 5.-Valor absoluto 6.-Mayor y menor que\n";
-
-        std::cin >> opc;
+    float valorAbsoluto = abs(valor);
 
 
+    while (repetir)
+    {
+    std::cout << "Hola usuario dime que Operacion quieres hacer?\n";
+    std::cout << "1.-suma 2.-resta 3.-multiplicacion 4.-division 5.-Valor absoluto 6.-Mayor y menor que\n";
+
+    std::cin >> opc;
+
+   
         switch (opc)
         {
         case 1:
@@ -23,33 +30,34 @@ int main()
             std::cin >> n1;
             std::cout << "Dame otro numero para sumarlo\n";
             std::cin >> n2;
-            std::cout << n1 + n2;
+            std::cout <<"El resultado es: " << n1 + n2;
             break;
         case 2:
             std::cout << "Dame un numero\n";
             std::cin >> n1;
             std::cout << "Dame otro numero para restarlo\n";
             std::cin >> n2;
-            std::cout << n1 - n2;
+            std::cout << "El resultado es: " << n1 - n2;
             break;
         case 3:
             std::cout << "Dame un numero\n";
             std::cin >> n1;
             std::cout << "Dame otro numero para multiplicarlo\n";
             std::cin >> n2;
-            std::cout << n1 * n2;
+            std::cout << "El resultado es: " << n1 * n2;
             break;
         case 4:
             std::cout << "Dame un numero\n";
             std::cin >> n1;
             std::cout << "Dame otro numero para dividirlo\n";
             std::cin >> n2;
-            std::cout << n1 / n2;
+            std::cout << "El resultado es: " << n1 / n2;
             break;
         case 5:
             std::cout << "Dame un numero para darte su valor absoluto\n";
-            std::cin >> VA;
-            std::cout << "El valor absoluto de "; std::cout << VA;  std::cout << " es: "; std::cout << valorabsoluto;
+            std::cin >> valor;
+            std::cout << "El valor absoluto de "; std::cout << valor;  std::cout << " es: "; std::cout << valorAbsoluto;
+            std::cin.ignore();
             break;
         case 6:
             std::cout << "Dame un numero: ";
@@ -72,8 +80,14 @@ int main()
                     std::cout << "El mayor es " << n1 << "\nY el menor es " << n2 << std::endl;
                 }
             }
-           
+            std::cin.ignore();
         }
+        std::cout << "\nQuieres hacer otra operacion?";
+        std::cout << "\n1.-Si  0.-No\n";
+        std::cin >> repetir;
+        system("cls");
+
+    }
 }
             
         
