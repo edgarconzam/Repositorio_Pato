3a5beec3abfc067f6f24264e09ab9c633c2257bb
edgarconzam <patricio.zambrano2005@gmail.com>
2023-10-03 20:08:00 -0600

    sd

--- prueba xd/prueba xd.cpp
+++ prueba xd/prueba xd.cpp
@@ -1,80 +1,24 @@
-// P011_Condicionales_AdivinaQue.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//Adivina que deporte
-//Edgar Constantino
+// P008 Bucles.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//Edgar Cosntantino
 
 #include <iostream>
-#include <string>
-
-using namespace std;
 
 int main()
 {
-    bool opc;
+   
     bool seguir = true;
 
-    while (seguir)
+
+    
+    do
     {
-        cout << "Bienvenido al juego donde yo trratare de adivinar el deporte que elijas" << endl;
-        cout << "Elije uno de estos deportes" << endl;
-        cout << "1.- Futbol 2.- Baloncesto 3.- Tenis 4.- futbol americano" << endl;
-        cout << "Yo Adivinare el deporte que hayas eligido con algunas preguntas" << endl;
-        cout << "Tu deporte es de contacto?" << endl;
-        cout << "1.- Si  0.- No" << endl;
-        cin >> opc;
-
-        if (opc)
-        {
-            cout << "Tu deporte se juega con las manos principalmente?" << endl;
-            cout << "1.- si 0.-No" << endl;
-            cin >> opc;
-            if (opc)
-            {
-                cout << "Tu deporte se juega en cancha grande?" << endl;
-                cout << "1.-Si 0.-No" << endl;
-                cin >> opc;
-                if (opc)
-                {
-                    cout << "Tu deporte es Futbol Americano" << endl;
-                }
-                else
-                {
-                    cout << "Tu deporte es el baloncesto" << endl;
-                }
-            }
-            else
-            {
-                cout << "Tu deporte es el futbol" << endl;
-
-            }
-        }
-        else
-        {
-            cout << "tu deporte es de mesa?" << endl;
-            cin >> opc;
-            if (opc)
-            {
-                cout << "tu deporte tiene actividad fisica?" << endl;
-                cin >> opc;
-                if (opc)
-                {
-                    cout << "tu deporte es pingpong" << endl;
-                }
-                else
-                {
-                    cout << "tu deporte es ajedrez" << endl;
-                }
-            }
-            else
-            {
-                cout << "tu deporte es tenis" << endl;
-            }
-                
-            
-        }
-        cout << "Deseas jugar otra vez? :D" << endl;
-        cout << "1.-Si 0.-No" << endl;
-        cin >> seguir;
-        system("cls");
-
+        std::cout << "Entraste al do-while...0.-No 1.-Si\n";
+        std::cin >> seguir;
+    } while (seguir);
+    
+    for (int i = 0; i <= 100; i = i + 1)
+    {
+        std::cout << "El contador va en: " << i;
+        std::cout << std::endl;
     }
 }
