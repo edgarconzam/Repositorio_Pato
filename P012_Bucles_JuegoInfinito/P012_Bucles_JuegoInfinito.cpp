0ba53401f0d406e8b2bdb8455bffd0cb0f4cfe92
Omar <omat.vader@hotmail.com>
2023-10-09 08:58:37 -0600

    p012

---
+++ P012_Bucles_JuegoInfinito/P012_Bucles_JuegoInfinito.cpp
@@ -0,0 +1,52 @@
+// P012_Bucles_JuegoInfinito.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//Edgar Constantino
+//Ciclo de juego infinito gastar 3 vidas, preguntar al usuario si quiere continuar
+//Contar hasta 100
+
+#include <iostream>
+#include <locale.h>
+
+int main()
+{
+    int contador = 0;
+    bool repetir;
+    bool opc;
+
+    setlocale(LC_ALL, "ES_MX.UTF-8");
+    while (repetir)
+    {
+        setlocale(LC_ALL, "ES_MX.UTF-8");
+        std::cout << "Hola bienvenido a LATAM Dia 1\n";
+        std::cout << "3 <3\n";
+        std::cout << "Te encuentras con un xolo que te la anda hacieno de pedo..... ¿Que haces?\n";
+        std::cout << "1.-Corres 0.-Te haces el valiente y lo enfrentas\n";
+        std::cin >> opc;
+
+        if (opc)
+        { 
+            std::cout << "Que qlo pero inteligente\n";  
+            std::cout << "Tienes 3 <3\n";
+        }
+        else
+        {
+            std::cout << "El xolo saca su navaja y te picotea\n";
+            std::cout << "Te quedan 2 <3\n";
+        }
+        
+        std::cout << "Dia 2\n";
+        std::cout << "*Spawneas en la playa de acapulco*\n";
+        std::cout << "¿Te metes al mar o te quedas en la arena escuchando las olas?\n";
+        std::cout << "1.-Me meto al mar 0.-Me quedo escuchando las olas\n";
+        std::cin >> opc;
+
+        if (opc)
+        {
+            std::cout << "Te metiste al mar y accidentalmente tragas agua y te da una infeccion estomacal muy fuerte y te mueres\n";
+            std::cout << "Tienes 3 <3\n";
+        }
+
+    }
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar p
