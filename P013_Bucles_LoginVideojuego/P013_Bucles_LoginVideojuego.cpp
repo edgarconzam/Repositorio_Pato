0b5d59d01a06429d2172a3f904e7e59ea5c52d54
edgarconzam <patricio.zambrano2005@gmail.com>
2023-10-15 13:22:27 -0600

    P013

--- P013_Bucles_LoginVideojuego/P013_Bucles_LoginVideojuego.cpp
+++ P013_Bucles_LoginVideojuego/P013_Bucles_LoginVideojuego.cpp
@@ -4,42 +4,87 @@
 //Si ingreso los datos correctos volver a pedir los datos.
 
 #include <iostream>
+#include <string>
 
 
 int main()
 {
-	std::string usuario;
-	std::string password;
-	bool repetir = true;
+	std::string user1;
+	std::string user2;
+	std::string password1;
+	std::string password2;
+	bool reinicio = true;
 
+	std::cout << "Bienvenido al valo\n";
+	std::cout << "Registrate\n";
+	std::cout << "Ingresa un nombre de usuario\n";
+	getline(std::cin, user1);
+	std::cout << "Ingresa tu password\n";
+	getline(std::cin, password1);
 	
-	
-		std::cout << "Bienvenido al Valo\n";
-		std::cout << "Ingresa tus datos\n";
-
-		do
+	while (reinicio)
+	{
+		std::cout << "Tienes que iniciar sesion para jugar valo\n";
+		std::cout << "Ingresa tu usuario\n";
+		
+		std::cin >> user2;
+		
+		if (user1 == user2)
 		{
-			std::cout << "User: \n";
-			std::cin >> usuario;
-			std::cout << "Password: \n";
-			std::cin >> password;
-
-			if (usuario == "Pato")
+		
+			std::cout << "Bien, Ingresa tu password\n";
+			std::cin >> password2;
+			
+			if (password1 == password2)
 			{
-				if (password == "edgar123")
-				{
-					
-				}
-				else
-				{
-					while (usuario != "Pato" || password != "elpepexd");
-				}
+			std::cout << "BIENVENIDO AL VALO\n";
 			}
 			else
 			{
-				while (usuario != "Pato" || password != "elpepexd");
+				while (password1 != password2)
+				{
+					std::cout << "El password es incorrecto\n";
+					std::cout << "Vuelve a intentarlo\n";
+					std::cin >> password2;
+					if (password1 == password2)
+					{
+						std::cout << "BIENVENIDO AL VALO, hora de rankear\n";
+					}
+				}
 			}
 		}
-	
+		else
+		{ 
+			while (user1 != user2)
+			{
+				std::cout << "El user es incorrecto\n";
+				std::cout << "Intentalo de nuevo\n";
+				std::cin >> user2;
+
+				if (user1 == user2)
+				{
+					std::cout << "Bien, ahora ingresa el password\n";
+					std::cin >> password2;
+					if (password1 == password2)
+					{
+						std::cout << "BIENVENIDO AL VALO, Hora de rankear\n";
+					}
+					else
+					{
+						while (password1 != password2)
+						{
+							std::cout << "El password esta incorrecto\n";
+							std::cout << "Intentalo de nuevo\n";
+							std::cin >> password2;
+							if (password1 == password2)
+							{
+								std::cout << "BIENVENIDO AL VALO, Hora de rankear\n";
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
